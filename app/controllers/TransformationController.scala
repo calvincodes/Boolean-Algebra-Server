1bab8a45c0c5495a4dd937c97250a049c4b1b5ac
calvincodes <arpitjain1821@gmail.com>
2018-12-09 11:34:56 -0600

    Adding documentation to TransformationController

--- app/controllers/TransformationController.scala
+++ app/controllers/TransformationController.scala
@@ -3,9 +3,16 @@ package controllers
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, ControllerComponents}
 
+/**
+  * This controller handles the request for transformation of boolean expressions into DNF and CNF forms
+  */
 @Singleton
 class TransformationController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
+  /**
+    * This method will be called when the application
+    * receives a `POST` request with a path of `/getDNF`.
+    */
   def getDNF = Action {request => {
     val json = request.body.asJson.get
     val expr = JSONUtil
@@ -15,6 +22,10 @@ class TransformationController @Inject() (cc: ControllerComponents) extends Abst
     Ok(TransformerFactory.transform(expr, TransformationType.DNF).toString)
   }}
 
+  /**
+    * This method will be called when the application
+    * receives a `POST` request with a path of `/getCNF`.
+    */
   def getCNF = Action {request => {
     val json = request.body.asJson.get
     val expr = JSONUtil
